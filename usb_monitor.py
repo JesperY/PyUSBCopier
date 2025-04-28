868020ee50ba1cfc80aafb88e5fffa1905668fd5
Jesper.Y <yjunpo@qq.com>
2025-04-28 21:47:15 +0800

    feat: only do copying when source path is newer than backup

--- usb_monitor.py
+++ usb_monitor.py
@@ -80,6 +80,8 @@ class USBMonitor:
             logger.info("User interrupted, stopping monitoring")
 
 def main():
+    # TODO windows 托盘
+    # TODO 日志文件按日期分割，只保留最近七天
     logger.info("Program started")
     monitor = USBMonitor()
     try:
